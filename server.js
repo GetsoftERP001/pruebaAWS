5a2c994aecf38770e2c4386b02a1c59b7d78833c
GetsoftERP0001 <jhonny.chamba@getsoft.com.ec>
2022-04-06 11:07:06 -0500

    Segundo Commit

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('<h1>hola mundo desde Express</h1'))
 
-app.listen(3000);
-console.log('Se esta ejecutando :)');
+app.listen(8080);
+console.log('Se esta ejecutando puerto 8080 :)');
