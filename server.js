f4da40bbfbae2bacdd3e9337f7d977b2a74f3c15
GetsoftERP0001 <jhonny.chamba@getsoft.com.ec>
2022-04-06 11:42:53 -0500

    Cuarto commit html JC

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get('/', (req, res) => res.send('<h1>hola mundo desde Express</h1'))
 
-app.listen(8080);
-console.log('Se esta ejecutando puerto 8080 :)');
+
+app.listen(8080, function () {
+    console.log('Se esta ejecutando puerto 8080 :)');
+});
